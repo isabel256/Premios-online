9782678a14621b232b71a4cfe2e0d7c2a924e61d
Isabel256 <orunareinaldo@hotmail.com>
2025-11-30 06:44:23 -0500

    Solucion del Cannot GET /

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "API en línea y funcionando correctamente.",
+        status: "OK"
+    });
+});
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB conectado"))
